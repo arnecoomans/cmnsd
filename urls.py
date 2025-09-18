e074cba555ba8470f6857068cc55a919378fe426
Arne Coomans <app+github@arnecoomans.nl>
2025-09-18 16:17:58 +0200

    add basic url scheme to either copy or include in project urls

--- urls.py
+++ urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'cmnsd'
 
 urlpatterns = [
-  path('json/<str:model>/', views.JsonDispatch.as_view(), name='dispatch'),
-
+  path('<str:model>/', views.JsonDispatch.as_view(), name='dispatch'),
+  path('<str:model>/<int:object_id>/', views.JsonDispatch.as_view(), name='dispatch_object_by_id'),
+  path('<str:model>/<int:object_id>-<str:object_slug>/', views.JsonDispatch.as_view(), name='dispatch_object_by_id_and_slug'),
+  path('<str:model>/<int:object_id>/<str:field>/', views.JsonDispatch.as_view(), name='dispatch_field_of_object_by_slug'),
+  path('<str:model>/<int:object_id>-<str:object_slug>/<str:field>/', views.JsonDispatch.as_view(), name='dispatch_field_of_object_by_id_and_slug'),
 ]
