d04ecfd00adc3e28d6005c682d385799d3bf68b5
Arne Coomans <app+github@arnecoomans.nl>
2025-09-18 16:14:32 +0200

    added basic templatetags

---
+++ templatetags/objreplace.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def strreplace(value, what, to):
+  """
+  Replace substring in value.
+  Usage in template:
+    {% strreplace "original string" "original" "new"}
+    {% strreplace app.url_format "{address}" location.address %}
+  """
+  return str(value).replace(str(what), str(to))
