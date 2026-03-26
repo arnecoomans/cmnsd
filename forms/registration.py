b7efa1d00a48482e164deccac7f9f1dda9e5070b
Arne Coomans <github@arnecoomans.nl>
2026-03-26 18:58:35 +0100

    improve translation format

--- forms/registration.py
+++ forms/registration.py
@@ -2,12 +2,13 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.utils.text import capfirst
 
 
 class RegistrationForm(UserCreationForm):
-  email = forms.EmailField(required=True, label=_('Email address'), widget=forms.EmailInput(attrs={'autofocus': True}))
-  first_name = forms.CharField(required=False, label=_('First name'), max_length=150)
-  last_name = forms.CharField(required=False, label=_('Last name'), max_length=150)
+  email = forms.EmailField(required=True, label=capfirst(_('email address')), widget=forms.EmailInput(attrs={'autofocus': True}))
+  first_name = forms.CharField(required=False, label=capfirst(_('first name')), max_length=150)
+  last_name = forms.CharField(required=False, label=capfirst(_('last name')), max_length=150)
 
   class Meta:
     model = User
